25d658f221f10e59bf97006d0f124667974aedf2
KSJaay <50052603+KSJaay@users.noreply.github.com>
2022-05-25 19:42:44 +0100

    Added global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { GlobalStyle } from "../styles/globals";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
