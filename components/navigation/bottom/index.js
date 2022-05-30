fcf60900a73dea912e01544de54d55a3c67315d1
KSJaay <50052603+KSJaay@users.noreply.github.com>
2022-05-31 00:25:51 +0100

    Update index.js

--- components/navigation/bottom/index.js
+++ components/navigation/bottom/index.js
@@ -1,12 +1,32 @@
 import styled from "styled-components";
+
 import Search from "./search";
 import Startup from "./startup";
+
+import Calendar from "./calendar";
+import Language from "./language";
+import Wifi from "./wifi";
+import Volume from "./volume";
+import Hidden from "./hiddenMenu";
 
 export default function BottomNavigation() {
   return (
     <Content>
-      <Startup />
-      <Search />
+      <Left>
+        <Startup />
+        <Search />
+      </Left>
+
+      <Center></Center>
+
+      <Right>
+        <Hidden />
+        <Volume />
+        <Wifi />
+        <Language />
+        <Calendar />
+        <Closer />
+      </Right>
     </Content>
   );
 }
@@ -14,6 +34,29 @@ export default function BottomNavigation() {
 const Content = styled.div`
   display: flex;
   height: 48px;
-  background-color: var(--secondary-color);
+  background-color: var(--secondary-color-opacity-90);
   width: 100%;
+`;
+
+const Left = styled.div`
+  display: flex;
+`;
+
+const Center = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const Right = styled.div`
+  display: flex;
+`;
+
+const Closer = styled.div`
+  height: 48px;
+  width: 4px;
+  border-left: 1px solid gray;
+
+  &:hover {
+    background-color: var(--light-color);
+  }
 `;
