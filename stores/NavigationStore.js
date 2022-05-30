a0ddc2f5afb8f421b99a9ebb441ebbaa1feb9362
KSJaay <50052603+KSJaay@users.noreply.github.com>
2022-05-31 00:23:54 +0100

    Store updates

--- stores/NavigationStore.js
+++ stores/NavigationStore.js
@@ -3,40 +3,49 @@ import { action, makeObservable, observable } from "mobx";
 export default class NavigationStore {
   constructor() {
     // Booleans to check which sections of the navbar are currently open
-    this.calendar = false;
-    this.language = false;
-    this.sound = false;
-    this.wifi = false;
-    this.windowsUpadte = false; // Icon for there's a windows update (Randomly appears)
-    this.startup = false; // Up arrow with applications that are currently open
-    this.weather = false;
-    this.menu = false;
+    this.options = {
+      calendar: false,
+      language: false,
+      sound: false,
+      wifi: false,
+      windowsUpadte: false, // Icon for there's a windows update (Randomly appears)
+      startup: false, // Up arrow with applications that are currently open
+      weather: false,
+      menu: false,
+    };
 
     makeObservable(this, {
-      calendar: observable,
-      calendar: observable,
-      language: observable,
-      sound: observable,
-      wifi: observable,
-      windowsUpadte: observable,
-      startup: observable,
-      weather: observable,
-      menu: observable,
-      openMenu: action,
+      options: observable,
+      toggleMenu: action,
     });
   }
 
-  openMenu = (name) => {
-    if (this[name]) {
-      this.calendar = false;
-      this.language = false;
-      this.sound = false;
-      this.wifi = false;
-      this.windowsUpadte = false;
-      this.startup = false;
-      this.weather = false;
-      this.menu = false;
-      this[name] = true;
+  toggleMenu = (name) => {
+    const options = Object.keys(this.options);
+    options.forEach((option) => {
+      if (option === name) {
+        this.options[option] = !this.options[option];
+      } else {
+        this.options[option] = false;
+      }
+    });
+  };
+
+  // This is a function that will be called when the user clicks
+  handleClick = (e) => {
+    if (
+      e.path.filter((x) => {
+        if (
+          x.className &&
+          x.className.length > 0 &&
+          x.className.includes("react-calendar")
+        ) {
+          return x;
+        }
+      }).length > 0 ||
+      e.target.id.includes("react-calendar")
+    ) {
+      this.toggleMenu("calendar");
     }
   };
 }
--- stores/RootStore.js
+++ stores/RootStore.js
@@ -1,5 +1,13 @@
+import { createContext } from "react";
+import ModalStore from "./ModalStore";
+import NavigationStore from "./NavigationStore";
+import UserStore from "./UserStore";
+
 export const rootStore = {
   baseStore: new Map(),
+  modalStore: new ModalStore(),
+  navigationStore: new NavigationStore(),
+  userStore: new UserStore(),
 };
 
 export const RootStoreContext = createContext(rootStore);
